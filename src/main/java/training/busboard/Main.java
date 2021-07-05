598a65b997259649831d0e4c9d27b4094b2f971d
nkadiadrian <adriannkadi@gmail.com>
2021-07-05 12:59:28 +0200

    Initialised Repository

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,12 @@
 package training.busboard;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
     }
 }	
