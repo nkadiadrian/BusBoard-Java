b45d1ce5f5e75896f4e00da818eaf190280b4abe
nkadiadrian <adriannkadi@gmail.com>
2021-07-05 17:59:17 +0200

    Fixed dependency version in pom.xml -> changed to 2.34 Implemented printing of sorted arrivals

--- src/main/java/training/busboard/Arrival.java
+++ src/main/java/training/busboard/Arrival.java
@@ -2,41 +2,46 @@ package training.busboard;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Arrival {
-    //private LocalDateTime expectedArrival;
+    private int minutesToArrival;
+    private Date expectedArrival;
     private int lineId;
-    private String lineName;
+    private String destinationName;
 
     public Arrival() {
     }
-//
-//    public Arrival(LocalDateTime expectedArrival, int lineId, String lineName) {
-//        this.expectedArrival = expectedArrival;
-//        this.lineId = lineId;
-//        this.lineName = lineName;
-//    }
-//
-//    public void setExpectedArrival(LocalDateTime expectedArrival) {
-//        this.expectedArrival = expectedArrival;
-//    }
+
+    public Arrival(Date expectedArrival, int lineId, String destinationName) {
+        this.expectedArrival = expectedArrival;
+        this.lineId = lineId;
+        this.destinationName = destinationName;
+    }
+
+    public void setExpectedArrival(Date expectedArrival) {
+        this.expectedArrival = expectedArrival;
+    }
 
     public void setLineId(int lineId) {
         this.lineId = lineId;
     }
 
-    public void setLineName(String lineName) {
-        this.lineName = lineName;
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+    public int getMinutesToArrival() {
+        Date currentDate = new Date();
+        minutesToArrival = Math.round((expectedArrival.getTime() - currentDate.getTime())/60000);
+        return minutesToArrival;
     }
 
     @Override
     public String toString() {
-        return "Arrival{" +
-                //"expectedArrival=" + expectedArrival +
-                ", lineId=" + lineId +
-                ", lineName='" + lineName + '\'' +
-                '}';
+        return "Expected Arrival in " + getMinutesToArrival() +
+                " mins: \t" + lineId +
+                " going to \t" + destinationName;
     }
 }
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,39 +1,58 @@
 package training.busboard;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import org.glassfish.jersey.jackson.JacksonFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String args[]) throws IOException {
+        String id = inputBusStopIDLoop();
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
 
-        // String jsonData = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/arrivals").request(MediaType.APPLICATION_JSON_TYPE).get(new GenericType<String>() {});
-
-//        List<Arrival> arrivals = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/arrivals")
-//                .request(MediaType.APPLICATION_JSON)
-//                .get(new GenericType<List<Arrival>>() {});
-
-        String arrivals = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/arrivals")
+        List<Arrival> arrivals = client.target("https://api.tfl.gov.uk/")
+                .path("StopPoint/{id}/arrivals")
+                .resolveTemplate("id", id)
                 .request(MediaType.APPLICATION_JSON)
-                .get(String.class);
+                .get(new GenericType<List<Arrival>>() {});
 
-        ObjectMapper objectMapper = new ObjectMapper();
-//
+        arrivals = arrivals.stream()
+                .sorted(Comparator.comparing(Arrival::getMinutesToArrival))
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arrivals.get(i));
+        }
+
+
+//        String arrivals = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/arrivals")
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(String.class);
+
+//        ObjectMapper objectMapper = new ObjectMapper();
+
 //        WebTarget myResource = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/arrivals");
 //        String response = myResource.request(MediaType.TEXT_PLAIN)
 //                .get(String.class);
 
-        List<Arrival> arrivalsList = objectMapper.readValue(arrivals, new TypeReference<List<Arrival>>(){});
-        System.out.println(arrivals);
+//        List<Arrival> arrivalsList = objectMapper.readValue(arrivals, new TypeReference<List<Arrival>>(){});
     }
-}	
+
+    private static String inputBusStopIDLoop() {
+        String id;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input Bus Stop ID: ");
+        id = scanner.nextLine();
+
+        return id;
+    }
+}
