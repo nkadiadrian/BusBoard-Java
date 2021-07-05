5ab1d656b29c5c317a3934bfcb812bfa831defd8
nkadiadrian <adriannkadi@gmail.com>
2021-07-05 16:05:32 +0200

    Ammended arrival to include ignore unknown property

--- src/main/java/training/busboard/Arrival.java
+++ src/main/java/training/busboard/Arrival.java
@@ -1,21 +1,27 @@
 package training.busboard;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.time.LocalDateTime;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Arrival {
-    private LocalDateTime expectedArrival;
+    //private LocalDateTime expectedArrival;
     private int lineId;
     private String lineName;
 
-    public Arrival(LocalDateTime expectedArrival, int lineId, String lineName) {
-        this.expectedArrival = expectedArrival;
-        this.lineId = lineId;
-        this.lineName = lineName;
+    public Arrival() {
     }
-
-    public void setExpectedArrival(LocalDateTime expectedArrival) {
-        this.expectedArrival = expectedArrival;
-    }
+//
+//    public Arrival(LocalDateTime expectedArrival, int lineId, String lineName) {
+//        this.expectedArrival = expectedArrival;
+//        this.lineId = lineId;
+//        this.lineName = lineName;
+//    }
+//
+//    public void setExpectedArrival(LocalDateTime expectedArrival) {
+//        this.expectedArrival = expectedArrival;
+//    }
 
     public void setLineId(int lineId) {
         this.lineId = lineId;
@@ -27,8 +33,8 @@ public class Arrival {
 
     @Override
     public String toString() {
-        return "Arrivals{" +
-                "expectedArrival=" + expectedArrival +
+        return "Arrival{" +
+                //"expectedArrival=" + expectedArrival +
                 ", lineId=" + lineId +
                 ", lineName='" + lineName + '\'' +
                 '}';
